90f2f04d430ac006eb0f38aadd45cda83d7f76c2
Wrxw <92599185+Wrxw@users.noreply.github.com>
2023-04-22 13:19:50 +0200

    Camera movement

---
+++ Gra/Assets/Scripts/PlaYER/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float sensX;
+    public float sensY;
+
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+}
